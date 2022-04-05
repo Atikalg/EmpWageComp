3630a65b8faf6ef2d14f200d6f8214c7683876c3
Atikalg <101569489+Atikalg@users.noreply.github.com>
2022-04-05 16:01:46 +0530

    Delete EmpWageComp.java

--- EmpWageComp.java
+++
@@ -1,8 +0,0 @@
-package com.bridgelab.Assignment4;
-
-public class EmpWageComp {
-	public static void main(String[] args) {
-		System.out.println("Welcome to Employee wage computation program");
-	}
-
-}
