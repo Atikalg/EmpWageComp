db12f3d986f744e04f03ecd4160d8f83cba2644b
atikalg <who.atik@gmail.com>
2022-04-05 16:10:37 +0530

    Added : calculating employee wage

---
+++ EmpWageCompUC2.java
@@ -0,0 +1,21 @@
+package com.bridgelab.Assignment4;
+
+public class EmpWageCompUC2 {
+
+	public static void main(String[] args) {
+		int isFullTime = 1;
+		int EmpRatePerHour =20;
+		// Variable
+		int empHrs = 0;
+		int empWage = 0;
+		// Calculation
+		double empCheck = Math.floor(Math.random() * 10) % 2;
+		if ( empCheck == isFullTime )
+			empHrs = 8;
+		else
+			empHrs = 0;
+		empWage = empHrs * EmpRatePerHours;
+		System.out.println("Employee wage : " + empWage);
+	}
+
+}
