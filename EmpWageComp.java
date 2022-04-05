b6ee8d73f6efeb65902aed3406c027070ebfbd65
atikalg <who.atik@gmail.com>
2022-04-05 15:47:09 +0530

    Added : welcome message

---
+++ EmpWageComp.java
@@ -0,0 +1,8 @@
+package com.bridgelab.Assignment4;
+
+public class EmpWageComp {
+	public static void main(String[] args) {
+		System.out.println("Welcome to Employee wage computation program");
+	}
+
+}
