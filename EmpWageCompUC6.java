14a2a2fc5b75b1527c82ce39bcc60b073e97cd56
atikalg <who.atik@gmail.com>
2022-04-06 18:26:53 +0530

    Added : calculating employee wage for month

---
+++ EmpWageCompUC6.java
@@ -0,0 +1,44 @@
+package com.bridgelab.Assignment4;
+/**
+ * @author atik
+ *
+ */
+public class EmpWageCompUC6 {
+		public static final int isPartTime = 1;
+		public static final int isFullTime = 2;
+		public static final int empRatePerHour = 20;
+		public static final int numOfWorkingDays = 20;
+		public static final int maxHrsInMonth = 100;
+		
+	public static void main(String[] args) {
+		//variables
+		int empHrs = 0;
+		int totalEmpHrs = 0;
+		int totalWorkingDays = 0;
+		//computation
+		while ((totalEmpHrs <= maxHrsInMonth) && (totalWorkingDays < numOfWorkingDays)) {
+			totalWorkingDays++;
+			int empCheck = (int) Math.floor(Math.random() * 10) % 3;
+			// Use switch case to check employee is full time, part time or absent
+			switch(empCheck) {
+			case isPartTime : System.out.println("Employee Present : Full time");
+				empHrs = 8;
+				break;
+			case isFullTime : System.out.println("Employee Present : Part time");
+				empHrs = 4;
+				break;
+			default : System.out.println("Employee is Absent");
+				empHrs = 0;
+		}
+			//calculation for employee wage
+			totalEmpHrs += empHrs;
+			System.out.println("Day : " + totalWorkingDays + " Employee hours : " + empHrs);
+		}
+		// calculating monthly wages
+		int totalEmpWage = totalEmpHrs * empRatePerHour;
+		System.out.println("Employee worked " + totalEmpHrs + " Hrs in " + totalWorkingDays + " days.");
+		System.out.println("Total Employee Wage : " + totalEmpWage);
+		
+	}
+	
+}
