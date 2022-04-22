d21b5dc51a5e53d13acaf9aeaa88f99d6e01de56
atikalg <who.atik@gmail.com>
2022-04-22 19:19:31 +0530

    Added : Employee wages calculation interface file

---
+++ ICompanyEmpWage.java
@@ -0,0 +1,6 @@
+package com.bridgelab.Assignment_10;
+
+public interface ICompanyEmpWage {
+	public void addCompanyEmpWage(String company, int empRatePerHr, int numOfWorkingDays, int maxHrsPerMonth);
+	 void empWage();
+}
